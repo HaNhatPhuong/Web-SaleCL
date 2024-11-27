6c30438516e885704291612a8a188a4900dc3a42
HaNhatPhuong <hnphuong216@gmail.com>
2024-11-27 11:21:54 +0800

    login update

--- src/components/ContentSideBar/Login/Login.jsx
+++ src/components/ContentSideBar/Login/Login.jsx
@@ -1,18 +1,52 @@
 import InputCommon from '@components/InputCommon/InputCommon';
 import styles from './styles.module.scss';
 import Button from '@components/Button/Button';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { Value } from 'sass';
 function Login() {
   const { container, title, boxRememberMe, lostPw } = styles;
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email('Invalid Email').required('Email is Required'),
+      password: Yup.string()
+        .min(6, 'Password must be at least 6 characters')
+        .required('Password is Required'),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
   return (
     <div className={container}>
       <div className={title}>Sign In</div>
-      <InputCommon label='Email' type='text' isRequired />
-      <InputCommon label='Password' type='password' isRequired />
-      <div className={boxRememberMe}>
-        <input type='checkbox' />
-        <span>Remember Me</span>
-      </div>
-      <Button content={'LOGIN'} />
+      <form onSubmit={formik.handleSubmit}>
+        <InputCommon
+          id='email'
+          label='Email'
+          type='text'
+          isRequired
+          formik={formik}
+        />
+
+        <InputCommon
+          id='password'
+          label='Password'
+          type='password'
+          isRequired
+          formik={formik}
+        />
+
+        <div className={boxRememberMe}>
+          <input type='checkbox' />
+          <span>Remember Me</span>
+        </div>
+        <Button content={'LOGIN'} type='submit' />
+      </form>
       <div className={lostPw}>Lost Your Password?</div>
     </div>
   );
--- src/components/InputCommon/InputCommon.jsx
+++ src/components/InputCommon/InputCommon.jsx
@@ -2,16 +2,21 @@ import { useState } from 'react';
 import styles from './styles.module.scss';
 import { FaEye } from 'react-icons/fa';
 import { IoIosEyeOff } from 'react-icons/io';
-function InputCommon({ label, type, isRequired = false }) {
-  const { labelInput, boxInput, container, boxIcon } = styles;
+function InputCommon({ label, type, isRequired = false, ...props }) {
+  const { labelInput, boxInput, container, boxIcon, errMsg } = styles;
+  const { formik, id } = props;
   const isPassword = type === 'password';
   const [showPassWord, setShowPassword] = useState(false);
+  const messageErr = formik.errors[id];
 
   const isShowTextPassword =
     type === 'password' && showPassWord ? 'text' : type;
   const handleShowPassword = () => {
     setShowPassword(!showPassWord);
   };
+
+  const isErr = formik.touched[id] && formik.errors[id];
+  // const messageErr = formik.errors[id];
   return (
     <div className={container}>
       <div className={labelInput}>
@@ -19,12 +24,19 @@ function InputCommon({ label, type, isRequired = false }) {
         {isRequired && <span>*</span>}
       </div>
       <div className={boxInput}>
-        <input type={isShowTextPassword} />
+        <input
+          type={isShowTextPassword}
+          {...props}
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          value={formik.values[id]}
+        />
         {isPassword && (
           <div className={boxIcon} onClick={handleShowPassword}>
             {showPassWord ? <IoIosEyeOff /> : <FaEye />}
           </div>
         )}
+        {isErr && <div className={errMsg}>{messageErr}</div>}
       </div>
     </div>
   );
